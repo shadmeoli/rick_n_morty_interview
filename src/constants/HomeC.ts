6671bc62c89b326216492e4c7dba56bf373dada3
shadmeoli <shadrack@shopzetu.com>
2024-02-15 13:18:47 +0300

    ✨ Character view page

--- src/constants/HomeC.ts
+++ src/constants/HomeC.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = "https://rickandmortyapi.com/api";
+export const GRAPH_URL = "https://rickandmortyapi.com/graphql"
--- src/pages/locations/[ID].tsx
+++ src/pages/locations/[ID].tsx
@@ -1,58 +1,75 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
-
-import { BASE_URL } from "@/constants/HomeC";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { BASE_URL, GRAPH_URL } from "@/constants/HomeC";
 import { Character } from "@/interfaces/HomeT";
+import { Input } from "@/components/ui/input";
 import { SkeletonLocation } from "@/components/ui/Skeleton";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 export default function CharcterByID() {
   const [loading, setLoading] = useState<boolean>(false);
-  const [location_ID, setLocationID] = useState<string>();
+  const [location_ID, setLocationID] = useState<number>();
   const [location_name, setLocationName] = useState<string>();
   const [characters, setCharacters] = useState<Character[]>();
   const [showAlert, setShowAlert] = useState<boolean>(false);
   const [value, setValue] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+  const client = new ApolloClient({
+    uri: GRAPH_URL,
+    cache: new InMemoryCache(),
+  });
 
   useEffect(() => {
     setLoading(true);
-    setLocationID(sessionStorage.getItem("location_id") as string);
-    setLocationName(sessionStorage.getItem("origin") as string);
-    async function loadData() {
-      try {
-        let response;
-        if (value) {
-          response = await axios.get(`${BASE_URL}/character?query=${value}`);
-        } else {
-          // Assuming location_name is already set properly
-          response = await axios.get(
-            `${BASE_URL}/character?location=${location_name}`,
-          );
-        }
-        setCharacters(response.data.results);
-      } catch (error) {
-        console.error("Error loading data:", error);
-      } finally {
-        setLoading(false); // Move setLoading(false) inside the finally block
-      }
-    }
-    loadData();
-  }, [value]); // Add value to the dependency array
 
-  const handleSearch = async () => {
-    setLoading(true); // Set loading to true before making the request
-    try {
-      const response = await axios.get(`${BASE_URL}/character?query=${value}`);
-      setCharacters(response.data.results);
-    } catch (error) {
-      console.error("Error searching characters:", error);
-    } finally {
-      setLoading(false); // Set loading to false after the request is completed
+    setLocationID(Number(sessionStorage.getItem("location_id")));
+    setLocationName(sessionStorage.getItem("origin") as string);
+    async function withGQL() {
+      const results = await client.query({
+        query: gql`
+          query GetCharacters {
+            characters(page: ${page}, filter: {name: "${value}"}) {
+              results {
+                id
+                name
+                image
+                status
+                gender
+                location {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        `,
+      });
+
+      setCharacters(results.data.characters.results);
+      console.log(results);
     }
-  };
+
+    withGQL();
+    setLoading(false);
+  }, [value, page]); // Add value to the dependency array
 
   return (
     <>
+      {/* <Input
+        className="w-60 border-black"
+        onChange={(event: Event) => setValue(event?.target?.value as string)}
+        placeholder="Character Name"
+      /> */}
       {/* Add loading indicator */}
       {loading && (
         <div className="flex w-full flex-wrap items-center justify-center gap-2">
@@ -76,18 +93,55 @@ export default function CharcterByID() {
                 src={character.image}
               />
               <section className="flex flex-col items-start space-y-2">
-                <h1 className="overflow-hidden truncate font-primary font-bold">
+                <h1 className="overflow-clip truncate font-primary font-bold">
                   {character.name}
                 </h1>
                 <p className="min-w-12 rounded-full bg-blue-200 p-1 text-center text-xs text-blue-800">
                   {character.status}
                 </p>
                 <p className="animate-pulse text-xs font-light">
-                  {location_name}
+                  {character.location.name}
                 </p>
               </section>
             </div>
           ))}
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious className="cursor-pointer" onClick={() => {
+                  if (page === 0) {
+                    return
+                  } else {
+                    setPage(page - 1)
+                  }
+                }} />
+              </PaginationItem>
+              {
+                page >= 4 && (
+              <PaginationItem>
+                <PaginationLink className="bg-pink-200 text-pink-800 cursor-pointer" onClick={() => setPage(1)} >1</PaginationLink>
+              </PaginationItem>
+                )
+              }
+              {page > 1 && (
+              <PaginationItem>
+                <PaginationLink className="cursor-pointer" onClick={() => setPage(page-1)} >{page-1}</PaginationLink>
+              </PaginationItem>
+              )}
+              <PaginationItem>
+                <PaginationLink isActive>{page}</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink className="cursor-pointer" onClick={() => setPage(page+1)}>{page+1}</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext className="cursor-pointer" onClick={() => setPage(page+1)} />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       )}
     </>
