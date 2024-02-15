53aecb461026ec41d1937491e34edc6bf340ba0a
shadmeoli <shadrack@shopzetu.com>
2024-02-15 10:12:58 +0300

    🐛 residence clean up

--- src/pages/locations/[ID].tsx
+++ src/pages/locations/[ID].tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import { BASE_URL } from "@/constants/HomeC";
 import { Character } from "@/interfaces/HomeT";
+import { SkeletonLocation } from "@/components/ui/Skeleton";
 
 export default function CharcterByID() {
   const [loading, setLoading] = useState<boolean>(false);
@@ -25,7 +26,7 @@ export default function CharcterByID() {
         } else {
           // Assuming location_name is already set properly
           response = await axios.get(
-            `${BASE_URL}/character?origin=${location_name}&location=${location_name}`,
+            `${BASE_URL}/character?location=${location_name}`,
           );
         }
         setCharacters(response.data.results);
@@ -53,23 +54,38 @@ export default function CharcterByID() {
   return (
     <>
       {/* Add loading indicator */}
-      {loading && <p>Loading...</p>}
+      {loading && (
+        <div className="flex w-full flex-wrap items-center justify-center gap-2">
+          {Array.from({ length: 4 }, (_, index) => (
+            <SkeletonLocation key={index} />
+          ))}
+        </div>
+      )}
 
-      {characters?.length > 0 && (
-        <div className="flex w-screen flex-wrap p-10 gap-4">
+      {characters?.length && (
+        <div className="flex w-screen flex-wrap gap-4 p-10">
           {characters?.map((character, index) => (
-            <div className=" rounded h-60 w-40 bg-gray-100 flex flex-col items-start justify-start p-2" key={index}>
+            <div
+              className=" flex h-60 w-40 flex-col items-start justify-start rounded bg-gray-100 p-2"
+              key={index}
+            >
               <Image
                 alt={character.name}
                 height={100}
                 width={200}
                 src={character.image}
               />
-             <section className="space-y-2 flex flex-col items-start">
-              <h1 className="truncate overflow-hidden font-primary font-bold">{character.name}</h1>
-              <p className="bg-blue-200 text-blue-800 rounded-full text-xs p-1 text-center min-w-12">{character.status}</p>
-              <p className="text-xs font-light animate-pulse">{location_name}</p>
-              </section> 
+              <section className="flex flex-col items-start space-y-2">
+                <h1 className="overflow-hidden truncate font-primary font-bold">
+                  {character.name}
+                </h1>
+                <p className="min-w-12 rounded-full bg-blue-200 p-1 text-center text-xs text-blue-800">
+                  {character.status}
+                </p>
+                <p className="animate-pulse text-xs font-light">
+                  {location_name}
+                </p>
+              </section>
             </div>
           ))}
         </div>
