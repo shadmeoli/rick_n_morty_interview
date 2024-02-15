01c1e7ce2b869325831c44aa997240f3ca9cb42b
shadmeoli <shadrack@shopzetu.com>
2024-02-15 14:25:20 +0300

    🎉 note saving

--- src/pages/locations/[ID].tsx
+++ src/pages/locations/[ID].tsx
@@ -16,9 +16,24 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { RocketIcon } from "@radix-ui/react-icons";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+import { api } from "@/utils/api";
 
 export default function CharcterByID() {
-  const router = useRouter()
+  const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
   const [location_ID, setLocationID] = useState<number>();
   const [location_name, setLocationName] = useState<string>();
@@ -26,6 +41,8 @@ export default function CharcterByID() {
   const [showAlert, setShowAlert] = useState<boolean>(false);
   const [value, setValue] = useState<string>("");
   const [page, setPage] = useState<number>(1);
+  const [characterName, setCharacterName] = useState<string>();
+  const [note, setNote] = useState<string>();
   const client = new ApolloClient({
     uri: GRAPH_URL,
     cache: new InMemoryCache(),
@@ -65,6 +82,25 @@ export default function CharcterByID() {
     setLoading(false);
   }, [value, page]); // Add value to the dependency array
 
+  const db = api.post.create.useMutation();
+
+  function saveNote() {
+    try {
+      db.mutate({
+        note: note,
+        character_name: characterName,
+      });
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setShowAlert(true)
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 3000);
+      setNote("");
+    }
+  }
+
   return (
     <>
       <section className="flex flex-col items-center -space-y-6">
@@ -78,8 +114,8 @@ export default function CharcterByID() {
         <p>A Simple site to show case my full stack skills</p>
       </section>
 
-      <div className="mt-16 flex w-[60%] flex-col items-start px-10 space-y-1">
-          <h1 className="font-primary font-semibold">Character Name</h1>
+      <div className="mt-16 flex w-[60%] flex-col items-start space-y-1 px-10">
+        <h1 className="font-primary font-semibold">Character Name</h1>
         <section className="flex flex-row space-x-2">
           <Input
             onChange={(event) => {
@@ -102,88 +138,134 @@ export default function CharcterByID() {
       )}
 
       {characters?.length && (
-        <div className="flex w-screen flex-wrap gap-4 p-10">
-          {characters?.map((character, index) => (
-            <div
-              className=" flex h-60 w-40 flex-col items-start justify-start rounded bg-gray-100 p-2"
-              key={index}
-            >
-              <Image
-                alt={character.name}
-                height={100}
-                width={200}
-                src={character.image}
-              />
-              <section className="flex flex-col items-start space-y-2">
-                <h1 className="w-36 text-ellipsis overflow-clip truncate font-primary font-bold">
-                  {character.name}
-                </h1>
-                <p className="min-w-12  rounded-full bg-blue-200 p-1 text-center text-xs text-blue-800">
-                  {character.status}
-                </p>
-                <p className="animate-pulse text-xs font-light">
-                  {character.location.name}
-                </p>
-              </section>
-            </div>
-          ))}
-          <Pagination>
-            <PaginationContent>
-              <PaginationItem>
-                <PaginationPrevious
-                  className="cursor-pointer"
-                  onClick={() => {
-                    if (page === 0) {
-                      return;
-                    } else {
-                      setPage(page - 1);
-                    }
-                  }}
-                />
-              </PaginationItem>
-              {page >= 4 && (
-                <PaginationItem>
-                  <PaginationLink
-                    className="cursor-pointer bg-pink-200 text-pink-800"
-                    onClick={() => setPage(1)}
-                  >
-                    1
-                  </PaginationLink>
-                </PaginationItem>
-              )}
-              {page > 1 && (
-                <PaginationItem>
-                  <PaginationLink
-                    className="cursor-pointer"
-                    onClick={() => setPage(page - 1)}
-                  >
-                    {page - 1}
-                  </PaginationLink>
-                </PaginationItem>
-              )}
-              <PaginationItem>
-                <PaginationLink isActive>{page}</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink
-                  className="cursor-pointer"
-                  onClick={() => setPage(page + 1)}
+        <Dialog>
+          <DialogTrigger>
+            <div className="flex w-screen flex-wrap gap-4 p-10">
+              {characters?.map((character, index) => (
+                <button
+                  className=" flex h-60 w-40 flex-col items-start justify-start rounded bg-gray-100 p-2"
+                  key={index}
+                  onClick={() => setCharacterName(character.name)}
                 >
-                  {page + 1}
-                </PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationNext
-                  className="cursor-pointer"
-                  onClick={() => setPage(page + 1)}
+                  <Image
+                    alt={character.name}
+                    height={100}
+                    width={200}
+                    src={character.image}
+                  />
+                  <section className="flex flex-col items-start space-y-2">
+                    <h1 className="w-36 overflow-clip truncate text-ellipsis font-primary font-bold">
+                      {character.name}
+                    </h1>
+                    <p className="min-w-12  rounded-full bg-blue-200 p-1 text-center text-xs text-blue-800">
+                      {character.status}
+                    </p>
+                    <p className="w-36 animate-pulse overflow-clip truncate text-ellipsis text-xs font-light">
+                      {character.location.name}
+                    </p>
+                  </section>
+                </button>
+              ))}
+              <Pagination>
+                <PaginationContent>
+                  <PaginationItem>
+                    <PaginationPrevious
+                      className="cursor-pointer"
+                      onClick={() => {
+                        if (page === 0) {
+                          return;
+                        } else {
+                          setPage(page - 1);
+                        }
+                      }}
+                    />
+                  </PaginationItem>
+                  {page >= 4 && (
+                    <PaginationItem>
+                      <PaginationLink
+                        className="cursor-pointer bg-pink-200 text-pink-800"
+                        onClick={() => setPage(1)}
+                      >
+                        1
+                      </PaginationLink>
+                    </PaginationItem>
+                  )}
+                  {page > 1 && (
+                    <PaginationItem>
+                      <PaginationLink
+                        className="cursor-pointer"
+                        onClick={() => setPage(page - 1)}
+                      >
+                        {page - 1}
+                      </PaginationLink>
+                    </PaginationItem>
+                  )}
+                  <PaginationItem>
+                    <PaginationLink isActive>{page}</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink
+                      className="cursor-pointer"
+                      onClick={() => setPage(page + 1)}
+                    >
+                      {page + 1}
+                    </PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationNext
+                      className="cursor-pointer"
+                      onClick={() => setPage(page + 1)}
+                    />
+                  </PaginationItem>
+                </PaginationContent>
+              </Pagination>
+            </div>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Setup Note</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  character's Name
+                </Label>
+                <Input disabled value={characterName} className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Note on {characterName}
+                </Label>
+                <Input
+                  onChange={(event) => setNote(event.target.value)}
+                  id="username"
+                  value={note}
+                  className="col-span-3"
                 />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
-        </div>
+              </div>
+            </div>
+            <DialogFooter>
+              <Button onClick={saveNote} type="submit">
+                Save Note
+              </Button>
+            </DialogFooter>
+            {showAlert && (
+              <Alert className="bg-green-200 font-primary text-black shadow-lg">
+                <RocketIcon className="h-4 w-4" />
+                <AlertTitle className="font-bold text-green-800">
+                  Heads up!
+                </AlertTitle>
+                <AlertDescription>Note has been saved</AlertDescription>
+              </Alert>
+            )}
+          </DialogContent>
+        </Dialog>
       )}
     </>
   );
